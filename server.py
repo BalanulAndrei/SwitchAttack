63c900faca4588af141181b6fc8a49b0c575c196
BalanulAndrei <andreibalanmusat779@gmail.com>
2026-07-16 18:14:52 +0300

    Fixed folder hierarchy

--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
-from mininet.net import Mininet
-from mininet.cli import CLI
+from topology import make_network
 
 app = Flask(__name__, static_folder="public")
 
-net = None
-sw = None
+global net,sw
 
 @app.route("/")
 def start():
@@ -34,35 +32,9 @@ def stop_switch():
     return "Switch script stopped via web interface."
 
 
-def setup_network():
-    global net, sw 
-    
-    net = Mininet()
-
-    h1 = net.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
-    h2 = net.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
-    h3 = net.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
-    
-    sw = net.addHost('sw', ip=None)
-
-    net.addLink(h1, sw) # sw-eth0
-    net.addLink(h2, sw) # sw-eth1
-    net.addLink(h3, sw) # sw-eth2
-
-    net.start()
-
-    # stop routing
-    sw.cmd('sysctl -w net.ipv4.ip_forward=0')
-    # deactivate IPv6
-    for node in net.hosts:
-        node.cmd('sysctl -w net.ipv6.conf.all.disable_ipv6=1')
-        node.cmd('sysctl -w net.ipv6.conf.default.disable_ipv6=1')
-
-    sw.cmd('python3 switch.py > /tmp/sw.log 2>&1 &')
-
 if __name__ == "__main__":
     print("Starting Mininet network in the background...")
-    setup_network()
+    net,sw = make_network()
     
     try:
         print("Network is running! Starting Flask server...")
--- topology.py
+++ topology.py
@@ -39,36 +39,32 @@ def customAddLink(host, switch):
 	
 
 def make_network():
-	pass
 
 
-	# net = Mininet()
+	net = Mininet()
 
-	# h1 = net.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
-	# h2 = net.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
-	# # h3 = net.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
+	h1 = net.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
+	h2 = net.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
+	# h3 = net.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
 	
-	# sw1 = net.addHost('sw1', ip=None)
+	sw1 = net.addHost('sw1', ip=None)
 
-	# net.addLink(h1,sw1) # sw1-eth0
-	# net.addLink(h2,sw1) # sw1-eth1
-	# # net.addLink(h3,sw1) # sw1-eth2
+	net.addLink(h1,sw1) # sw1-eth0
+	net.addLink(h2,sw1) # sw1-eth1
+	# net.addLink(h3,sw1) # sw1-eth2
 
-	# net.start()
+	net.start()
 
-	# # stop routing
-	# sw1.cmd('sysctl -w net.ipv4.ip_forward=0')
-	# # deactivate IPv6
-	# for node in net.hosts:
-	# 	node.cmd('sysctl -w net.ipv6.conf.all.disable_ipv6=1')
-	# 	node.cmd('sysctl -w net.ipv6.conf.default.disable_ipv6=1')
+	# stop routing
+	sw1.cmd('sysctl -w net.ipv4.ip_forward=0')
+	# deactivate IPv6
+	for node in net.hosts:
+		node.cmd('sysctl -w net.ipv6.conf.all.disable_ipv6=1')
+		node.cmd('sysctl -w net.ipv6.conf.default.disable_ipv6=1')
 
-	# sw1.cmd('python3 switch.py > /tmp/sw1.log 2>&1 &')
-	
-	# CLI(net)
-	
-	# sw1.cmd('kill %python3')
-	# net.stop()
+	sw1.cmd('python3 switch.py > /tmp/sw1.log 2>&1 &')
+
+	return net, sw1
 
 if __name__ == '__main__':
 	make_network()
